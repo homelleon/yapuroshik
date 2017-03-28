5f93911ccaf938738b57a104828e513bacd189f6
homelleon <ihomelleoni@yandex.ru>
2017-03-28 18:59:16 +0300

    Artcicle entity created

---
+++ src/AppBundle/Entity/Article.php
@@ -0,0 +1,177 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * News blog entity.
+ * 
+ * @ORM\Entity
+ * @ORM\Table(name="article") *
+ * 
+ */
+class Article 
+{
+     /**
+     * @ORM\Column(type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+    
+     /**
+     * @ORM\Column(type="string", length=100)
+     */
+    private $name;
+    
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
+    private $author;
+    
+     /**
+     * @ORM\Column(type="string", length=100)
+     */
+    private $theme;
+    
+     /**
+     * @ORM\Column(type="integer")
+     */
+    private $image_id;
+    
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $description;
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return Article
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set author
+     *
+     * @param string $author
+     *
+     * @return Article
+     */
+    public function setAuthor($author)
+    {
+        $this->author = $author;
+
+        return $this;
+    }
+
+    /**
+     * Get author
+     *
+     * @return string
+     */
+    public function getAuthor()
+    {
+        return $this->author;
+    }
+
+    /**
+     * Set theme
+     *
+     * @param string $theme
+     *
+     * @return Article
+     */
+    public function setTheme($theme)
+    {
+        $this->theme = $theme;
+
+        return $this;
+    }
+
+    /**
+     * Get theme
+     *
+     * @return string
+     */
+    public function getTheme()
+    {
+        return $this->theme;
+    }
+
+    /**
+     * Set imageId
+     *
+     * @param integer $imageId
+     *
+     * @return Article
+     */
+    public function setImageId($imageId)
+    {
+        $this->image_id = $imageId;
+
+        return $this;
+    }
+
+    /**
+     * Get imageId
+     *
+     * @return integer
+     */
+    public function getImageId()
+    {
+        return $this->image_id;
+    }
+
+    /**
+     * Set description
+     *
+     * @param string $description
+     *
+     * @return Article
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * Get description
+     *
+     * @return string
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+}
