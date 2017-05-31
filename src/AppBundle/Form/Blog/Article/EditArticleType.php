7396d7df801e90074544772314ca0e3be2496c81
homelleon <ihomelleoni@yandex.ru>
2017-05-31 12:29:31 +0300

    correct namespaces
    - correct namespace in news_edit.html.tiwg
    - correct namespace in EditArticleType.php

--- src/AppBundle/Form/Blog/Article/EditArticleType.php
+++ src/AppBundle/Form/Blog/Article/EditArticleType.php
@@ -4,7 +4,7 @@ namespace AppBundle\Form\Blog\Article;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Blogger\BlogBundle\Form\Article\ArticleType;
+use AppBundle\Form\Blog\Article\ArticleType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 /**
